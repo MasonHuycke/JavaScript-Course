dd835272146349ab6ac26f004b79642ff0422f9e
Mason Huycke <huyckemason@gmail.com>
2022-03-14 23:05:01 -0400

    2022-03-14

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -90,4 +90,62 @@ const yearsUntilRetirement = function (birthYear, firstName) {
   //   return retirement;
   return `${firstName} retires in ${retirement} years`;
 };
+
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(3, 4, 5));
+
+const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win! (${avgDolphins} vs. ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win! (${avgKoalas} vs. ${avgDolphins})`);
+  } else {
+    console.log(`No team wins :(`);
+  }
+};
+checkWinner(scoreDolphins, scoreKoalas);
 */
+
+const friend1 = `Michael`;
+const friend2 = `Steven`;
+const friend3 = `Peter`;
+
+const friends = [`Michael`, `Steven`, `Peter`];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = `Jay`;
+console.log(friends);
+
+const jonas = [`Jonas`, `Huycke`, 2037 - 1991, `teacher`, friends];
+console.log(jonas);
+
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]),
+];
+console.log(ages);
